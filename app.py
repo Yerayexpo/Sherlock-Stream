098d4e3dec5583faa6f65f3c9ac4e065d1f73bf2
Yerayexpo <yerayexpo@gmail.com>
2023-11-08 11:15:24 +0100

    sherlock

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import streamlit.components.v1 as components
+import subprocess
+import sys
+
+
+st.set_page_config(page_title='Sherlock 🔎', page_icon = 'https://user-images.githubusercontent.com/27065646/53551960-ae4dff80-3b3a-11e9-9075-cef786c69364.png')
+col1, col2, col3 = st.columns([1, 8, 1])
+with col2:
+    st.title('Sherlock Username Search')
+
+st.image('https://cdn.leonardo.ai/users/d293df0e-cdbb-4d97-b51d-09f318b25c99/generations/134b5b0f-9577-4f34-994a-d0243c9d1a3a/Leonardo_Diffusion_XL_A_chaotic_and_cluttered_office_filled_wi_1.jpg')
+
+username = st.text_input('Username', value='', help='Enter the username you want to track')
+if st.button('Submit'):
+    st.text('Searching for \''+username+'\'... (this might take a minute)')
+    python_executable = sys.executable
+    try:
+        s = subprocess.check_output([python_executable, "sherlock", username])
+        st.text(s.decode("utf-8"))
+    except subprocess.CalledProcessError as e:
+        st.error(f"Error occurred: {e}")
